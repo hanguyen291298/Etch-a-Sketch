88298b9a6ec7debb74d91f876910a0d318446b77
hanguyen291298 <thuyha291298@gmail.com>
2023-09-07 14:54:53 +0700

    Create grid square (16x16)

---
+++ index.js
@@ -0,0 +1,15 @@
+let grid_r = 16;
+let grid_c = 16;
+const GRID_CONTAINER = document.querySelector(".grid-container");
+
+
+for (let i = 1; i <= grid_r; i++){
+    let grid_row = document.createElement("div") 
+    grid_row.classList.add("grid-row")
+    GRID_CONTAINER.appendChild(grid_row)
+    for(let j = 1; j <= grid_c; j++){
+        let new_div = document.createElement("div");
+        new_div.classList.add("grid-child");
+        grid_row.appendChild(new_div);
+    }
+}
