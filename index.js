592c3b25d400aea0237811c712ad2c4d300e1e8a
hanguyen291298 <thuyha291298@gmail.com>
2023-09-08 19:58:42 +0700

    Create effection to show background palette

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-// Create Pickr instance
+// Create Pickr instances
 
 const pickr = Pickr.create({
     el: '.color-picker',
+    theme: 'classic',
+    components: {
+
+    // Main components
+    preview: true,
+    opacity: true,
+    hue: true,
+
+    // Input / output Options
+    interaction: {
+    hex: true,
+    rgba: true,
+    hsla: true,
+    hsva: true,
+    cmyk: true,
+    input: true,
+    clear: true,
+    save: true,
+}
+}
+});
+
+const pickr_grid = Pickr.create({
+    el: '.background-picker',
     theme: 'classic',
     components: {
 
@@ -78,20 +102,62 @@ pickr.on("save", (color)=>{
     SELECTED_COLOR = color.toHEXA().toString();    
 })
 
+
 // Create effection to divs that the mouse over
 
 function effect(element){
     element.addEventListener("mouseover", () => {
         element.style.backgroundColor = SELECTED_COLOR;       
     })
+
 }
+
+// get the selected color background and make a function to change its color
+
+const background_color = document.querySelector(".grid-container")
+
+pickr_grid.on("save", (color)=>{
+    let color_selected = color.toHEXA().toString();
+    background_color.style.backgroundColor = color_selected    
+})
 
 // Create a function to clear the sketch every time the player press the clear button
 const clear_button = document.querySelector(".clear")
 
 function clear(){
     UpdateSize()
+    background_color.style.backgroundColor = "white"
+
 }
 
 clear_button.addEventListener("click", clear)
+
+
+
+// create a funtion to make grid lines
+const grid_lines = document.querySelectorAll(".grid-child")
+function show_grid(tyle){
+    grid_lines.forEach(element => {
+        element.style.borderStyle = tyle
+    });
+}
+// Create effection to the switch when it's active and show grid lines
+
+const toggle = document.getElementById("toggle")
+
+
+toggle.addEventListener("click", ()=>{
+    if (toggle.classList.contains("active")){
+        toggle.classList.remove("active")
+        toggle.classList.add("off")
+        show_grid("none")
+        
+    }
+    else{
+        toggle.classList.remove("off")
+        toggle.classList.add("active")
+        show_grid("ridge")
+        })
+    }
+})
 
